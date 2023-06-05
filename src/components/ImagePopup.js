74102668ec61f5fc6d93dfdd2724640a1cffffef
BuyanauskasAA <Buyanauskas@gmail.com>
2023-06-05 17:46:37 +0300

    fix: использовать тернарный оператор

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,8 +1,15 @@
 function ImagePopup({ card, onClose }) {
+  const isEmptyCard = Object.keys(card).length === 0;
+
   return (
-    <div className={`popup image-popup ${card && 'popup_opened'}`}>
+    <div className={`popup image-popup ${isEmptyCard ? '' : 'popup_opened'}`}>
       <figure className="image-popup__container">
-        <button onClick={onClose} className="button popup__close-button" type="button" aria-label="Закрыть"></button>
+        <button
+          onClick={onClose}
+          className="button popup__close-button"
+          type="button"
+          aria-label="Закрыть"
+        ></button>
         <img className="image-popup__image" src={card && card.link} alt={card && card.name} />
         <figcaption className="image-popup__caption">{card && card.name}</figcaption>
       </figure>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,8 +1,13 @@
 function PopupWithForm({ name, title, children, isOpened, onClose }) {
   return (
-    <div className={`popup ${name} ${isOpened && 'popup_opened'}`}>
+    <div className={`popup ${name} ${isOpened ? 'popup_opened' : ''}`}>
       <div className="popup__container">
-        <button onClick={onClose} className="button popup__close-button" type="button" aria-label="Закрыть"></button>
+        <button
+          onClick={onClose}
+          className="button popup__close-button"
+          type="button"
+          aria-label="Закрыть"
+        ></button>
         <h2 className="popup__title">{title}</h2>
         <form className="popup__form" name={name} noValidate>
           {children}
